67dbcda8ca989ad543f39673d03828cc07d9ffbf
Markus Andresen <markua@stud.ntnu.no>
2016-03-04 15:15:50 +0100

    doc added

--- src/senzorpack/CalculateSeconds.java
+++ src/senzorpack/CalculateSeconds.java
@@ -1,27 +1,26 @@
-package test;
-
 public class CalculateSeconds {
 	
-	/*
+	/**
 	 * declaration of variables
 	 */
 	public double distance;
 	public double speed;
-	
-	/*
+
+	/**
 	 * sets distance and speed variables
-	 * @param distance
-	 * @param speed
+	 * @param distance distance to car in front
+	 * @param speed of the vehicle
 	 */
 	public CalculateSeconds(double distance, double speed){
 		checkSpeed(speed); 
 		checkDistance(distance);
 	}
-	
-	/*
+
+
+	/**
 	 * tells if warning should be displayed
-	 * @param distance
-	 * @param speed
+	 * @param distance distance to car in front
+	 * @param speed speed the car holds
 	 * @return if warning should be displayed
 	 */
 	public boolean warning(double distance, double speed){
@@ -35,17 +34,17 @@ public class CalculateSeconds {
 		}
 	}
 	
-	/*
+	/**
 	 * sets distance variable
-	 * @param distance
+	 * @param distance distance to car in front
 	 */
 	private void setDistance(double distance){
 		this.distance = distance;
 	}
 	
-	/*
+	/**
 	 * checks if speed is over 20kph, converts speed to m/s and sets value
-	 * @param speed
+	 * @param speed speed the car holds
 	 * @throws IllegalArgumentException if speed is below 20kph
 	 */
 	private void checkSpeed(double speed){
@@ -57,9 +56,9 @@ public class CalculateSeconds {
 		}
 	}
 	
-	/*
+	/**
 	 * checks if distance is positive, sets distance or throws argument
-	 * @param distance
+	 * @param distance distance to car in front
 	 * @throws IllegalArgumentException if distance is negative
 	 */
 	private void checkDistance(double distance){
@@ -71,42 +70,41 @@ public class CalculateSeconds {
 		}
 	}
 	
-	/*
+	/**
 	 * sets speed
-	 * @param speed
+	 * @param speed car speed
 	 */
 	private void setSpeed(double speed){
 		this.speed = speed;
 	}
 	
-	/*
+	/**
 	 * returns distance
-	 * @param distance
 	 * @return distance
 	 */
 	private double getDistance(){
 		return this.distance;
 	}
 	
-	/*
+	/**
 	 * returns speed
-	 * @param speed
 	 * @return speed
 	 */
 	private double getSpeed(){
 		return this.speed;
 	}
 	
-	/*
+	/**
 	 * converts speed form kph to m/s
-	 * @param speedinKph
+	 * @param speedInKph speed in Kph
 	 * @return speed in m/s
 	 */
 	public double convertSpeedToMs(double speedInKph){
 		return speedInKph/3.6;
 	}
 
-	/*
+
+	/**
 	 * runs the code
 	 */
 	public static void main(String [] args){
