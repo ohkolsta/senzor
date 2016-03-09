4841426ce1adb91f876b390e327e4be4a869e2a8
ohkolsta <ohkolsta@stud.ntnu.no>
2016-03-09 11:57:05 +0100

    Added readData.java

---
+++ src/senzorpack/ReadData.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+
+
+public class ReadData {
+	String hvordan = "LINK TIL HVORDAN GJØRE DET: http://stackoverflow.com/questions/7467568/parsing-json-from-url";
+	String UrlString = "";
+
+	private static String readUrl(String UrlString) throws Exception {
+	    BufferedReader reader = null;
+	    try {
+	        URL url = new URL(UrlString);
+	        reader = new BufferedReader(new InputStreamReader(url.openStream()));
+	        StringBuffer buffer = new StringBuffer();
+	        int read;
+	        char[] chars = new char[1024];
+	        while ((read = reader.read(chars)) != -1)
+	            buffer.append(chars, 0, read); 
+
+	        return buffer.toString();
+	    } finally {
+	        if (reader != null)
+	            reader.close();
+	    }
+	}
+
+	
+	public static void Main(String[] args) throws Exception{
+		ReadData rd = new ReadData();
+		rd.readUrl("http://openxcplatform.com.s3.amazonaws.com/traces/nyc/uptown-west2.json");
+	}
+
+}
