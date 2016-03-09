009a31047226269d4d1b9f67f95fccdff98dff0c
Tonje Roås Mikalsen <tonjerm@gmail.com>
2016-03-09 17:51:32 +0100

    Lagt til kode som leser fart fra fil

--- src/senzorpack/ReadData.java
+++ src/senzorpack/ReadData.java
@@ -6,45 +6,32 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 
-
 public class ReadData {
 
-    public void read() {
+	public void read() {
 
-        int[] tall = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        BufferedReader br = null;
+		
 
-        try {
+		try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))){
 
-            String sCurrentLine;
+			String sCurrentLine;
 
-            br = new BufferedReader(new FileReader("data.txt"));
+			while ((sCurrentLine = br.readLine()) != null) {
+				if (sCurrentLine.startsWith("{\"name\":\"vehicle_speed\",\"value\":")) {
+					sCurrentLine = sCurrentLine.substring(32);
+					System.out.println(Double.valueOf(sCurrentLine.substring(0,
+							sCurrentLine.indexOf(','))));
+				}
+			}
 
-            while ((sCurrentLine = br.readLine()) != null) {
-                if(sCurrentLine.startsWith("{\"name\":\"vehicle_speed\",\"value\":")) {
-                    for (int q = 0; q < sCurrentLine.length(); q++) {
-                        System.out.println(sCurrentLine);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-                    }
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (br != null)br.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-
-    }
-
-    public static void main(String[] args) throws Exception  {
-        ReadData reader = new ReadData();
-        reader.read();
-    }
-
+	public static void main(String[] args) throws Exception {
+		ReadData reader = new ReadData();
+		reader.read();
+	}
 
 }
