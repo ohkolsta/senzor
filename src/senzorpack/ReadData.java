42a57656f62c8d368c475c0d17006a35e4db476b
Thomas Kleiven <thomas@isvik.no>
2016-03-09 15:50:24 +0100

    Leser nå alle vehicle_speed-linjene fra fil

--- src/senzorpack/ReadData.java
+++ src/senzorpack/ReadData.java
@@ -1,29 +1,50 @@
-        import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.net.URL;
+/**
+ * Created by thomaskleiven on 09.03.2016.
+ */
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 
 public class ReadData {
 
-    public void URLreader() throws Exception {
+    public void read() {
 
+        int[] tall = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        BufferedReader br = null;
 
+        try {
 
-        URL oracle = new URL("http://openxcplatform.com.s3.amazonaws.com/traces/nyc/uptown-west2.json");
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(oracle.openStream()));
+            String sCurrentLine;
 
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
-        in.close();
+            br = new BufferedReader(new FileReader("data.txt"));
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                if(sCurrentLine.startsWith("{\"name\":\"vehicle_speed\",\"value\":")) {
+                    for (int q = 0; q < sCurrentLine.length(); q++) {
+                        System.out.println(sCurrentLine);
+
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
     }
-
-
 
     public static void main(String[] args) throws Exception  {
         ReadData reader = new ReadData();
-        reader.URLreader();
+        reader.read();
     }
-}
 
+
+}
