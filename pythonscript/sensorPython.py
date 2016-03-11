fa48bd21583c768fb8fa0fb0101799e24ad36aa7
ohkolsta <ohkolsta@stud.ntnu.no>
2016-03-11 20:15:04 +0100

    modified python script

--- pythonscript/sensorPython.py
+++ pythonscript/sensorPython.py
@@ -1,89 +1,60 @@
-#!/usr/bin/python
-
-# remember to change the GPIO values below to match your sensors
-# GPIO output = the pin that's connected to "Trig" on the sensor
-# GPIO input = the pin that's connected to "Echo" on the sensor
+from sys import exit
+import time
+import RPi.GPIO as GPIO
 
 def reading(sensor):
-	import time
-	import RPi.GPIO as GPIO
-	
-	# Disable any warning message such as GPIO pins in use
-	GPIO.setwarnings(False)
-	
-	# use the values of the GPIO pins, and not the actual pin number
-	# so if you connect to GPIO 25 which is on pin number 22, the 
-	# reference in this code is 25, which is the number of the GPIO 
-	# port and not the number of the physical pin
-	GPIO.setmode(GPIO.BCM)
-	
-	if sensor == 0:
-		
-		# point the software to the GPIO pins the sensor is using
-		# change these values to the pins you are using
-		# GPIO output = the pin that's connected to "Trig" on the sensor
-		# GPIO input = the pin that's connected to "Echo" on the sensor
-		GPIO.setup(17,GPIO.OUT)
-		GPIO.setup(27,GPIO.IN)
-		GPIO.output(17, GPIO.LOW)
-		
-		# found that the sensor can crash if there isn't a delay here
-		# no idea why. If you have odd crashing issues, increase delay
-		time.sleep(0.3)
-		
-		# sensor manual says a pulse ength of 10Us will trigger the 
-		# sensor to transmit 8 cycles of ultrasonic burst at 40kHz and 
-		# wait for the reflected ultrasonic burst to be received
-		
-		# to get a pulse length of 10Us we need to start the pulse, then
-		# wait for 10 microseconds, then stop the pulse. This will 
-		# result in the pulse length being 10Us.
-		
-		# start the pulse on the GPIO pin 
-		# change this value to the pin you are using
-		# GPIO output = the pin that's connected to "Trig" on the sensor
-		GPIO.output(17, True)
-		
-		# wait 10 micro seconds (this is 0.00001 seconds) so the pulse
-		# length is 10Us as the sensor expects
-		time.sleep(0.00001)
-		
-		# stop the pulse after the time above has passed
-		# change this value to the pin you are using
-		# GPIO output = the pin that's connected to "Trig" on the sensor
-		GPIO.output(17, False)
+ 
+    GPIO.setwarnings(False)
+ 
+    GPIO.setmode(GPIO.BCM)
+ 
+    if sensor == 0:
 
-		# listen to the input pin. 0 means nothing is happening. Once a
-		# signal is received the value will be 1 so the while loop
-		# stops and has the last recorded time the signal was 0
-		# change this value to the pin you are using
-		# GPIO input = the pin that's connected to "Echo" on the sensor
-		while GPIO.input(27) == 0:
-		  signaloff = time.time()
-		
-		# listen to the input pin. Once a signal is received, record the
-		# time the signal came through
-		# change this value to the pin you are using
-		# GPIO input = the pin that's connected to "Echo" on the sensor
-		while GPIO.input(27) == 1:
-		  signalon = time.time()
-		
-		# work out the difference in the two recorded times above to 
-		# calculate the distance of an object in front of the sensor
-		timepassed = signalon - signaloff
-		
-		# we now have our distance but it's not in a useful unit of
-		# measurement. So now we convert this distance into centimetres
-		distance = timepassed * 17000
-		
-		# return the distance of an object in front of the sensor in cm
-		return distance
-		
-		# we're no longer using the GPIO, so tell software we're done
-		GPIO.cleanup()
+        GPIO.setup(17,GPIO.OUT)
+        GPIO.setup(22,GPIO.IN)
+        GPIO.output(17, GPIO.LOW)
 
-	else:
-		print "Incorrect usonic() function varible."
+        signaloff = 0.0
+        signalon = 0.0 
+ 
+        GPIO.output(17, True)
 
-		
-print reading(0)
+        time.sleep(0.00001)
+ 
+        GPIO.output(17, False)
+
+        while GPIO.input(22) == 0:
+            signaloff = time.time()
+
+        signalon = time.time()
+
+        #while GPIO.input(24) == 1:
+            #if (time.time() - signaloff) * 17000 > 400: #Max range of 4m
+                #raise Exception
+
+        signalon = time.time()
+ 
+        timepassed = signalon - signaloff
+
+        distance = timepassed * 17000
+ 
+        return distance
+
+        GPIO.cleanup()
+ 
+    else:
+        print "Incorrect usonic() function varible."
+
+counter = 0 
+while True:
+    try:
+        if counter < 10:
+            print reading(0)
+        else:
+            print "returned 10 values. exiting.."
+            exit()
+
+        counter = counter + 1
+    except Exception:
+        print ("Signal timed out")
+    time.sleep(1)
